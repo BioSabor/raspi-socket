840e191d35511c8a03262882a4e454ccf1c2956e
José Manuel Martínez Camacho <jose.mc.2004@gmail.com>
2022-05-06 14:34:08 +0200

    Rutas

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
                     resp, status = api.sendDataAPI()
                     if status==200:
                         con.clearBuffer()
+                        con.deleteAllRecord()
                         
                         if resp == 1:
                             con.setTime()
