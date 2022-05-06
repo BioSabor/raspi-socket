8d46d73793d76915c01cf71499f1c8753c7f9411
alberto-nieto <52299979+alberto-nieto@users.noreply.github.com>
2022-05-06 11:56:54 +0200

    GitIgnore

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ if __name__ == '__main__':
         else:
             print("Hay datos")
             # Procesamos los datos recibidos
-            df = con.processData(request, True) # Procesamos los datos recibidos
-            #Vaciar de datos el facial
-            
+            df = con.processData(request, True) # Procesamos los datos recibidos          
             
             #Intentamos enviar datos a la API
             if con.checkBuffer:
@@ -41,35 +39,3 @@ if __name__ == '__main__':
         print('No se ha podido conectar con el facial')
         print(e)
         # Agragar error a log de errores
-        
-        
-        
-    
-
-
-
-
-        # Si es correcto
-        #Limpiamos buffer
-
-        # Si no es correcto
-        #Intentamos despues
-
-
-    # Si hay datos, guardarlos en local
-
-
-
-    # Si todo va bien enviar vaciado de datos
-
-    # Enviar datos a API
-
-
-    
-    # print(request)
-
-    # # Guardar los datos en el archivo
-    # con.writeData(request, 'data.txt')
-    # # con.writeData(request, "./data.txt")
-    # #api.sendDataAPI(api.jsonConvert(request)) # Enviamos los datos a la API
-    # print("Terminado")
