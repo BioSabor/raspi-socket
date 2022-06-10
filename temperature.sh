d72a98344435d619b9703dc74766e023e057a6b5
adanillox <94868066+adanillox@users.noreply.github.com>
2022-06-10 12:35:41 +0200

    .

--- temperature.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-# Guardamos el valor de temperatura de la CPU a una variable
-cpuTemp=$(cat /sys/class/thermal/thermal_zone0/temp)
-
-curl http://app.biosabor.com:8088/v1/fincas/temperatura/$((cpuTemp/1000))
